0dc4cfbdb46e7243ec7f4946364e39fbad3e7c83
saberberdey3 <saberchino3@gmail.com>
2023-03-16 12:18:02 -0700

    New_Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(char), 59, stderr);
+	return (1);
 }
+
