af2c7571f324f6e3c701fbb4b8cbadbd3c99d37b
saberberdey3 <saberchino3@gmail.com>
2023-03-16 19:47:52 +0100

    Quote_2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,4 +9,3 @@ int main(void)
     fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return (1);
 }
-
