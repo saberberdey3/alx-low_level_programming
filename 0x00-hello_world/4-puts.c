14764e5f2f095205bcd26d18d1041348b8fb000a
saberberdey3 <saberchino3@gmail.com>
2023-03-16 17:16:45 +0100

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*Main
+*
+*Return
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}gcc: fatal error: no input files
+compilation terminated.
+
