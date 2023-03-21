41a7afc265e6963fb4072d21d0de9dc4e2dd0a42
saberberdey3 <saberchino3@gmail.com>
2023-03-21 16:54:07 +0000

    Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,18 @@
 
 void jack_bauer(void)
 {
-	int i, m;
+int i, m;
 
-	for (i = 0; i < 24; i++)
-	{
-		for (m = 0; m < 60; m++)
-		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (i = 0; i < 24; i++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
 }	
