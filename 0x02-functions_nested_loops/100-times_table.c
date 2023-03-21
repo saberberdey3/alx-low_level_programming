c16c3fb2c2eee28bfd9c848a25a7ab21908d7b1d
saberberdey3 <saberchino3@gmail.com>
2023-03-21 22:57:53 +0000

    Times_tobli

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,19 +7,26 @@
 
 void print_times_table(int n)
 {
-    int i, j, result;
-    char buffer[10];
+	int i, j, result;
 
-    if (n < 0 || n > 15) {
-        return;
-    }
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
-            result = i * j;
-            sprintf(buffer, ", %3d", result);
-            printf("%s", j == 0 ? "" : buffer);
-        }
-        printf("\n");
-    }
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				printf("%2d", result);
+			else if (result < 10)
+				printf(",   %d", result);
+			else if (result > 9 && result < 100)
+				printf(",  %d", result);
+			else if (result > 99)
+				printf(", %d", result);
+
+		}
+		printf("\n");
+	}
 }
