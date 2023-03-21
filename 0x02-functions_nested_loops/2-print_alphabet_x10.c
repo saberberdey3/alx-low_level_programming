6ae24bf11248bdbb797382f9c796ab327d74657e
saberberdey3 <saberchino3@gmail.com>
2023-03-21 12:38:21 +0000

    Your time is limited

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - a function that prints 10 times the alphabet
+*
+* Return: x10 a-z
+*/
+
+void print_alphabet_x10(void)
+{
+	int z, ba;
+
+	z = -1;
+
+	while (++z < 10)
+	{
+		for (ba = 'a'; ba <= 'z'; ba++)
+			{
+				_putchar(ba);
+			}
+			_putchar('\n');
+	}
+}
