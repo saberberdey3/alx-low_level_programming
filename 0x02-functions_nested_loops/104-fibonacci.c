c00cb268ea00b4bd005763ca1b58e5c579e78a6f
saberberdey3 <saberchino3@gmail.com>
2023-03-21 23:58:24 +0000

    Fibonacci_3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,24 +9,25 @@
 
 int main(void)
 {
-	unsigned int long i, a, b, c;
-	unsigned long int a1, a2, b1, b2, c1, c2;
+    int i;
+    unsigned long int a1 = 0, a2 = 1, b1 = 0, b2 = 2, c;
 
-	a = 1;
-	b = 2;
-	printf("%lu, %lu, ", a, b);
+    for (i = 0; i < 98; i++)
+    {
+        if (i == 0)
+            printf("%lu", a2);
+        else
+            printf("%lu", b2);
 
-	for (i = 2; i < 98; ++i)
-	{
-		c = a + b;
-		printf("%lu", c);
+        if (i != 97)
+            printf(", ");
 
-		if (i != 97)
-			printf(", ");
+        c = b1 + b2;
+        b1 = a2;
+        a2 = b2;
+        b2 = c;
+    }
+    printf("\n");
 
-		a = b;
-		b = c;
-	}
-	printf("\n");
-	return (0);
+    return (0);
 }
