507fbdd00c0aee925140f604a27be9d2bf353da3
Saber Berdey <84582255+saberberdey3@users.noreply.github.com>
2023-03-21 23:07:23 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,28 +5,12 @@
  * @n: number of the times table
  */
 
-void print_times_table(int n)
-{
-	int i, j, result;
-
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
-			if (j == 0)
-				printf("%2d", result);
-			else if (result < 10)
-				printf(",   %d", result);
-			else if (result > 9 && result < 100)
-				printf(",  %d", result);
-			else if (result > 99)
-				printf(", %d", result);
-
-		}
-		printf("\n");
-	}
+void print_times_table(int n) {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            printf("%d,", i * j);
+        }
+        printf("\n");
+    }
 }
+
