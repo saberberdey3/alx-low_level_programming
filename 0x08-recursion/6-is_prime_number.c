c3bbe37a1c6e4a0212344ac1371a3577efa1e11f
Saber Berdey <saberchino3@gmail.com>
2023-04-06 01:24:32 +0000

    Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,14 +9,14 @@
  */
 int is_prime(int n, int i)
 {
-    if (n < 2 || n % i == 0)
-        return (0);
-    else if (i == n / 2)
-        return (1);
-    else if (n % i != 0)
-        return (is_prime(n, i + 1));
-    else
-        return (0);
+	if (n < 2 || n % i == 0)
+	return (0);
+	else if (i == n / 2)
+	return (1);
+	else if (n % i != 0)
+	return (is_prime(n, i + 1));
+	else
+	return (0);
 }
 
 /**
@@ -27,10 +27,10 @@ int is_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-    if (n == 2)
-        return (1);
-    else if (n <= 1)
-        return (0);
-    else
-        return (is_prime(n, 2));
+	if (n == 2)
+	return (1);
+	else if (n <= 1)
+	return (0);
+	else
+	return (is_prime(n, 2));
 }
