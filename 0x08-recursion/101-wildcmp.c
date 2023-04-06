b16331777ec76428e41f1c76406bdf24c0be39fb
Saber Berdey <saberchino3@gmail.com>
2023-04-06 01:39:44 +0000

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,21 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered identical, otherwise 0
+ * wildcmp - compares two strings
+ * @s1: the first string to compare
+ * @s2: the second string containing '*' or not
  *
- * @s1: first string to compare
- * @s2: second string to compare
- *
- * Return: 1 if strings can be considered identical, otherwise 0
+ * Return: 1 if the strings are considered identical, and 0 if not
  */
-
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-	{
-		if (*s1 == '\0' && *(s2 + 1) == '\0')
-			return (1);
-		else if (*s1 == '\0' || *(s2 + 1) == '\0')
-			return (0);
-		else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-	}
-	else if (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-			return (1);
-		else if (wildcmp(s1 + 1, s2 + 1))
-			return (1);
-	}
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
