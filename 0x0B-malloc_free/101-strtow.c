b79073e8ff39249529a4648be2b123489c7b3e0f
Saber Berdey <saberchino3@gmail.com>
2023-04-11 23:55:04 +0000

    Easy_Task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,5 @@
-include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * count_word - helper function to count the number of words in a string
@@ -16,19 +16,20 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
+	if (s[c] == ' ')
+		flag = 0;
+	else if (flag == 0)
+	{
+		flag = 1;
+		w++;
 	}
+}
 
 	return (w);
 }
+
 /**
- * **strtow - splits a string into words
+ * strtow - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings (Success)
@@ -39,36 +40,39 @@ char **strtow(char *str)
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
 
-	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	matrix = malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+	if (str[i] == ' ' || str[i] == '\0')
+	{
+		if (c)
 		{
-			if (c)
-			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
-					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
-			}
-		}
-		else if (c++ == 0)
-			start = i;
+		end = i;
+		tmp = malloc(sizeof(char) * (c + 1));
+		if (tmp == NULL)
+			return (NULL);
+		while (start < end)
+			*tmp++ = str[start++];
+		*tmp = '\0';
+		matrix[k] = tmp - c;
+		k++;
+		c = 0;
+	}
+	}
+	else if (c++ == 0)
+		start = i;
 	}
 
 	matrix[k] = NULL;
