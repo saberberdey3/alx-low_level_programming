de24b77e47c5cff6c1b25a59111122b7bb62b66a
Saber Berdey <saberchino3@gmail.com>
2023-04-11 23:48:34 +0000

    Easy_Task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,85 +1,52 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
 /**
- * count_words - Counts the number of words in a string
- * @str: The string to count the words in
+ * **strtow - splits a string into words
+ * @str: string to split
  *
- * Return: The number of words in the string
- */
-int count_words(char *str)
-{
-    int i, words = 0;
-
-    for (i = 0; str[i] != '\0'; ++i)
-    {
-        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-        {
-            ++words;
-        }
-    }
-
-    return (words);
-}
-
-/**
- * strtow - Splits a string into words
- * @str: The string to split
- *
- * Return: A pointer to an array of strings (words)
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-    char **words;
-    int i, j, k, len, word_count = 0;
+	char **words = NULL, *token = NULL;
+	int count = 0;
 
-    if (str == NULL || str[0] == '\0')
-    {
-        return (NULL);
-    }
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-    word_count = count_words(str);
+	token = strtok(str, " ");
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, " ");
+	}
 
-    words = malloc(sizeof(char *) * (word_count + 1));
-    if (words == NULL)
-    {
-        return (NULL);
-    }
+	if (count == 0)
+		return (NULL);
 
-    for (i = 0, j = 0; i < word_count; ++i, ++j)
-    {
-        while (str[j] == ' ')
-        {
-            ++j;
-        }
+	words = malloc(sizeof(char *) * (count + 1));
+	if (words == NULL)
+		return (NULL);
 
-        len = 0;
-        while (str[j + len] != ' ' && str[j + len] != '\0')
-        {
-            ++len;
-        }
+	count = 0;
+	token = strtok(str, " ");
+	while (token != NULL)
+	{
+		words[count] = strdup(token);
+		if (words[count] == NULL)
+		{
+			while (count-- >= 0)
+				free(words[count]);
+			free(words);
+			return (NULL);
+		}
+		count++;
+		token = strtok(NULL, " ");
+	}
+	words[count] = NULL;
 
-        words[i] = malloc(sizeof(char) * (len + 1));
-        if (words[i] == NULL)
-        {
-            for (k = 0; k < i; ++k)
-            {
-                free(words[k]);
-            }
-
-            free(words);
-            return (NULL);
-        }
-
-        strncpy(words[i], &str[j], len);
-        words[i][len] = '\0';
-
-        j += len;
-    }
-
-    words[i] = NULL;
-
-    return (words);
+	return (words);
 }
