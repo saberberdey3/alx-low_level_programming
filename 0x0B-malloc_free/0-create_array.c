ea3b432f23ec080cf542c0dbea88c2c29a9b1f18
saberberdey3 <saberchino3@gmail.com>
2023-04-11 04:24:54 -0700

    EASY_Task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - create an array chars and initialize it 
+ * @size: the size of the array to create
+ * @c: the char to initialize the array
+ *
+ * Return: On success, a pointer to the created array. On faillur, NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	
+	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr [i] = c;
+
+	return (arr);
+}
